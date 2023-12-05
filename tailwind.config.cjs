533257972cc09520595ca06e1ace146af17cb552
imekachi <imekachi@gmail.com>
2023-12-05 17:01:52 +0700

    feat: add theme style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-primary': 'hsl(var(--color-background-primary) / <alpha-value>)',
+        'foreground-primary': 'hsl(var(--color-foreground-primary) / <alpha-value>)',
+        'background-input-primary': 'hsl(var(--color-background-input-primary) / <alpha-value>)',
+        'foreground-input-primary': 'hsl(var(--color-foreground-input-primary) / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
